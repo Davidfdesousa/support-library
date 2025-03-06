7feed22604140c06b0a0b2797577ddd89c1258d4
davidfdesousa <davidfdesousa@hotmail.com>
2025-03-06 17:44:45 -0300

    adjusts structure

--- stories/fonts/Page.stories.ts
+++ stories/fonts/Page.stories.ts
@@ -4,7 +4,7 @@ import { within, userEvent, expect } from '@storybook/test';
 import { Page } from './Page';
 
 const meta = {
-  title: 'Fonts/Page',
+  title: 'Fonts/Instalation',
   component: Page,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
@@ -15,10 +15,10 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const LoggedOut: Story = {};
+export const Web: Story = {};
 
 // More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
-export const LoggedIn: Story = {
+export const Mobile: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     const loginButton = canvas.getByRole('button', { name: /Log in/i });
--- stories/fonts/Page.tsx
+++ stories/fonts/Page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Header } from '../Header';
+import { Header } from '../components/Header';
 import '../page.css';
 
 type User = {
@@ -68,6 +68,7 @@ export const Page: React.FC = () => {
           Viewports addon in the toolbar
         </div>
       </section>
+      
     </article>
   );
 };
