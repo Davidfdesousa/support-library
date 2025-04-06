b474fd5919529ea6282e1fb4afa77b10b24e9966
David Ferreira <davidfdesousa@hotmail.com>
2025-04-06 12:34:17 -0300

    fix tsconfig, add react-jsx

--- dist/ui/src/button.js
+++ dist/ui/src/button.js
@@ -1,13 +1,10 @@
 "use strict";
 "use client";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Button = void 0;
-var react_1 = __importDefault(require("react"));
+var jsx_runtime_1 = require("react/jsx-runtime");
 var Button = function (_a) {
     var children = _a.children, className = _a.className, appName = _a.appName;
-    return (react_1.default.createElement("button", { className: className, onClick: function () { return alert("Hello from your ".concat(appName, " app!")); } }, children));
+    return ((0, jsx_runtime_1.jsx)("button", { className: className, onClick: function () { return alert("Hello from your ".concat(appName, " app!")); }, children: children }));
 };
 exports.Button = Button;
