75b913d403046c8bb0d56dbbfcfd6360b1613f54
David Ferreira <davidfdesousa@Leticias-MacBook-Pro.local>
2025-04-05 22:15:01 -0300

    adjust build to style-dictionary

--- packages/tokens/build-tokens.cjs
+++ packages/tokens/build-tokens.cjs
@@ -1,30 +1,36 @@
-// build-tokens.cjs
 const StyleDictionary = require('style-dictionary');
 
-const config = {
-  source: ['foundation/**/*.json'],
-  platforms: {
-    css: {
-      transformGroup: 'css',
-      buildPath: 'dist/css/',
-      files: [
-        {
-          destination: 'variables.css',
-          format: 'css/variables'
-        }
-      ]
-    },
-    js: {
-      transformGroup: 'js',
-      buildPath: 'dist/js/',
-      files: [
-        {
-          destination: 'tokens.js',
-          format: 'javascript/es6'
-        }
-      ]
-    }
-  }
-};
+const themes = ['light', 'dark'];
 
-StyleDictionary.extend(config).buildAllPlatforms();
+themes.forEach((theme) => {
+  const SD = StyleDictionary.extend({
+    source: [
+      'foundation/global/**/*.json',
+      `foundation/themes/${theme}.json`
+    ],
+    platforms: {
+      css: {
+        transformGroup: 'css',
+        buildPath: 'dist/css/',
+        files: [
+          {
+            destination: `variables.${theme}.css`,
+            format: 'css/variables'
+          }
+        ]
+      },
+      js: {
+        transformGroup: 'js',
+        buildPath: 'dist/js/',
+        files: [
+          {
+            destination: `tokens.${theme}.js`,
+            format: 'javascript/es6'
+          }
+        ]
+      }
+    }
+  });
+
+  SD.buildAllPlatforms();
+});
